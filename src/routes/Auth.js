eb7c408d0c401942a3a3498605db8da561476bc0
sonys95 <sonys95@naver.com>
2023-10-16 13:27:05 +0900

    231016

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,16 @@
 import React from "react";
 
+const Auth = () => (
+  <div>
+    <from>
+      <input type="text" placeholder="Email"></input>
+      <input type="password" placeholder="Password"></input>
+      <input type="submit" value="Log In"></input>
+    </from>
+    <div>
+      <button>Continue with Google</button>
+    </div>
+  </div>
+);
+
 export default () => <sapn>Auth</sapn>;
